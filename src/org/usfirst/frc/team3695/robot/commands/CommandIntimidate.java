4ef8c872b530136186075938f27f5a9f9b92060f
wh1ter0se <ipad3000@yahoo.com>
2017-02-11 23:28:51 -0600

    Casted value to double.

--- src/org/usfirst/frc/team3695/robot/commands/CommandIntimidate.java
+++ src/org/usfirst/frc/team3695/robot/commands/CommandIntimidate.java
@@ -25,22 +25,22 @@ public class CommandIntimidate extends Command {
 			int mode = interval.nextInt(3);
 			if (mode == 0) {
 				// short pulse
-				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((i/10)^2)/100); Thread.sleep(7); }
-				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((i/10)^2)/100); Thread.sleep(4); }
+				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(7); }
+				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(4); }
 			}
 			else if (mode == 1) {
 				// long pulse
-				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((i/10)^2)/100); Thread.sleep(5); }
+				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(5); }
 				Thread.sleep(500);
-				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((i/10)^2)/100); Thread.sleep(5); }
+				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(5); }
 			}
 			else {
 				// double short pulse
-				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((i/10)^2)/100); Thread.sleep(3); }
-				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((i/10)^2)/100); Thread.sleep(2); }
+				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(3); }
+				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(2); }
 				
-				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((i/10)^2)/100); Thread.sleep(4); }
-				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((i/10)^2)/100); Thread.sleep(2); }
+				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(4); }
+				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(2); }
 			}
 		} catch (InterruptedException e) { e.printStackTrace(); }
 		
