849fc94579f993b1f17cf2c09c35d4b84bb7af6f
wh1ter0se <ipad3000@yahoo.com>
2017-02-12 19:02:01 -0600

    Implemented CommandShootTime.

--- src/org/usfirst/frc/team3695/robot/commands/CommandIntimidate.java
+++ src/org/usfirst/frc/team3695/robot/commands/CommandIntimidate.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3695.robot.commands;
 
 import java.util.Random;
 
+import org.usfirst.frc.team3695.robot.commands.CommandShootTime;
 import org.usfirst.frc.team3695.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -20,29 +21,45 @@ public class CommandIntimidate extends Command {
 
     protected void execute() {
     	Random interval = new Random();
-    	try {
-			Thread.sleep(interval.nextInt(1000));
-			int mode = interval.nextInt(3);
-			if (mode == 0) {
-				// short pulse
-				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(7); }
-				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(4); }
-			}
-			else if (mode == 1) {
-				// long pulse
-				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(5); }
-				Thread.sleep(500);
-				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(5); }
-			}
-			else {
-				// double short pulse
-				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(3); }
-				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(2); }
-				
-				for (int i = 1; i <= 100; i++) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(4); }
-				for (int i = 100; i > 0; i--) { Robot.subsystemShooter.spin(((((double)i)/10.00)*((double)i)/10.00)/100); Thread.sleep(2); }
-			}
-		} catch (InterruptedException e) { e.printStackTrace(); }
+    	new CommandShootTime(interval.nextInt(1000), 0);
+		int mode = interval.nextInt(3);
+		if (mode == 0) {
+			// short pulse
+			for (int i = 1; i <= 100; i++) { 
+				double speed = Math.pow(((double)i)/10, 2)/100;
+				new CommandShootTime(7, speed);}
+			for (int i = 100; i > 0; i--) { 
+				double speed = Math.pow(((double)i)/10, 2)/100;
+				new CommandShootTime(4, speed);}
+			Robot.subsystemShooter.spin(0);
+		}
+		else if (mode == 1) {
+			// long pulse
+			for (int i = 1; i <= 100; i++) { 
+				double speed = Math.pow(((double)i)/10, 2)/100;
+				new CommandShootTime(5, speed);}
+			new CommandShootTime(500, 1);
+			for (int i = 100; i > 0; i--) { 
+				double speed = Math.pow(((double)i)/10, 2)/100;
+				new CommandShootTime(5, speed);}
+			Robot.subsystemShooter.spin(0);
+		}
+		else {
+			// double short pulse
+			for (int i = 1; i <= 100; i++) { 
+				double speed = Math.pow(((double)i)/10, 2)/100;
+				new CommandShootTime(3, speed);}
+			for (int i = 100; i > 0; i--) { 
+				double speed = Math.pow(((double)i)/10, 2)/100;
+				new CommandShootTime(2, speed);}
+			for (int i = 1; i <= 100; i++) { 
+				double speed = Math.pow(((double)i)/10, 2)/100;
+				new CommandShootTime(4, speed);}
+			for (int i = 100; i > 0; i--) { 
+				double speed = Math.pow(((double)i)/10, 2)/100;
+				new CommandShootTime(2, speed);}
+			Robot.subsystemShooter.spin(0);
+		}
 		
     }
 
