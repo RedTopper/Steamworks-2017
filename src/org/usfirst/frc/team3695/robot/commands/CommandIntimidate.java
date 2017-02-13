0beb32061a6e8409d71eec547cf0bfcbb096fec7
wh1ter0se <ipad3000@yahoo.com>
2017-02-12 19:05:28 -0600

    Added Switch Statement.

--- src/org/usfirst/frc/team3695/robot/commands/CommandIntimidate.java
+++ src/org/usfirst/frc/team3695/robot/commands/CommandIntimidate.java
@@ -23,42 +23,41 @@ public class CommandIntimidate extends Command {
     	Random interval = new Random();
     	new CommandShootTime(interval.nextInt(1000), 0);
 		int mode = interval.nextInt(3);
-		if (mode == 0) {
-			// short pulse
-			for (int i = 1; i <= 100; i++) { 
-				double speed = Math.pow(((double)i)/10, 2)/100;
-				new CommandShootTime(7, speed);}
-			for (int i = 100; i > 0; i--) { 
-				double speed = Math.pow(((double)i)/10, 2)/100;
-				new CommandShootTime(4, speed);}
-			Robot.subsystemShooter.spin(0);
-		}
-		else if (mode == 1) {
-			// long pulse
-			for (int i = 1; i <= 100; i++) { 
-				double speed = Math.pow(((double)i)/10, 2)/100;
-				new CommandShootTime(5, speed);}
-			new CommandShootTime(500, 1);
-			for (int i = 100; i > 0; i--) { 
-				double speed = Math.pow(((double)i)/10, 2)/100;
-				new CommandShootTime(5, speed);}
-			Robot.subsystemShooter.spin(0);
-		}
-		else {
-			// double short pulse
-			for (int i = 1; i <= 100; i++) { 
-				double speed = Math.pow(((double)i)/10, 2)/100;
-				new CommandShootTime(3, speed);}
-			for (int i = 100; i > 0; i--) { 
-				double speed = Math.pow(((double)i)/10, 2)/100;
-				new CommandShootTime(2, speed);}
-			for (int i = 1; i <= 100; i++) { 
-				double speed = Math.pow(((double)i)/10, 2)/100;
-				new CommandShootTime(4, speed);}
-			for (int i = 100; i > 0; i--) { 
-				double speed = Math.pow(((double)i)/10, 2)/100;
-				new CommandShootTime(2, speed);}
-			Robot.subsystemShooter.spin(0);
+		switch (mode) {
+			case 0: // short pulse
+				for (int i = 1; i <= 100; i++) { 
+					double speed = Math.pow(((double)i)/10, 2)/100;
+					new CommandShootTime(7, speed);}
+				for (int i = 100; i > 0; i--) { 
+					double speed = Math.pow(((double)i)/10, 2)/100;
+					new CommandShootTime(4, speed);}
+				Robot.subsystemShooter.spin(0);
+				break;
+			case 1: // long pulse
+				for (int i = 1; i <= 100; i++) { 
+					double speed = Math.pow(((double)i)/10, 2)/100;
+					new CommandShootTime(5, speed);}
+				new CommandShootTime(500, 1);
+				for (int i = 100; i > 0; i--) { 
+					double speed = Math.pow(((double)i)/10, 2)/100;
+					new CommandShootTime(5, speed);}
+				Robot.subsystemShooter.spin(0);
+				break;
+			case 2: // double short pulse
+				for (int i = 1; i <= 100; i++) { 
+					double speed = Math.pow(((double)i)/10, 2)/100;
+					new CommandShootTime(3, speed);}
+				for (int i = 100; i > 0; i--) { 
+					double speed = Math.pow(((double)i)/10, 2)/100;
+					new CommandShootTime(2, speed);}
+				for (int i = 1; i <= 100; i++) { 
+					double speed = Math.pow(((double)i)/10, 2)/100;
+					new CommandShootTime(4, speed);}
+				for (int i = 100; i > 0; i--) { 
+					double speed = Math.pow(((double)i)/10, 2)/100;
+					new CommandShootTime(2, speed);}
+				Robot.subsystemShooter.spin(0);
+				break;
 		}
 		
     }
