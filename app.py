4b0092d90550082bdb66b3bfbcb855d0b374ab4d
Jason Wilcox <wilcoxjaca0@gmail.com>
2017-02-17 13:48:15 -0600

    Sketchy Server Only
    * Removing old code, adding new python streaming and recording. It works.
    
    * Update README.md
    
    Yup
    
    * Code for RasPi before it gets bagged. This is just a stream

--- app.py
+++ app.py
@@ -27,20 +27,22 @@ def gen(camera):
         # Gets a frame from Camera.py
         frame = camera.get_frame()
 
+        ##recFrame = cv2.resize(frame, (1280,720), interpolation = cv2.INTER_LINEAR)
         # Any OpenCV image editing that is on the video should go here
-        if recording:
-            cv2.putText(frame,"Recording", (15,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,255,255], 2)
-            cv2.putText(frame,time.strftime("%I-%M-%S"), (1100,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,255,255], 1)
+        ##if recording:
+          ##  cv2.putText(frame,"Recording", (15,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,255,255], 2)
+          ##  cv2.putText(recFrame,time.strftime("%I-%M-%S"), (1100,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,255,255], 1)
 
+        cv2.putText(frame,"Score", (15,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,255,255], 2)
         # Write the frame to the video
-        out.write(frame)
+        ##out.write(recFrame)
 
         # Resize the frame so it can be streamed efficently
-        frame = cv2.resize(frame, (680,420), interpolation = cv2.INTER_LINEAR)
+        frame = cv2.resize(frame, (640, 480), interpolation = cv2.INTER_LINEAR)
 
         # If the recording is done, release the file. This is nessisary to not have corrupt video files
-        if recording is False:
-            out.release()
+        ##if recording is False:
+          ##  out.release()
 
         # Streaming code (MJPG Stream)
         success, jpeg = cv2.imencode('.jpg', frame)
@@ -76,25 +78,25 @@ def camera_recording_process_change():
     global out, recording, currFile
 
     # Tag the change in the console
-    print ('record process changed....')
+    print ('This function is turned off...')
 
-    if recording:
+    ##if recording:
 
         # Disable recording
-        recording = False
-        print ('recording turned off')
+      ##  recording = False
+      ##  print ('recording turned off')
         
-    else:
+    ##else:
 
         # Enable recording
-        recording = True
+      ##  recording = True
 
         # Set current file global, defines where the video will be saved
-        currFile = time.strftime("%I-%M-%d") + ".mp4"
+      ##  currFile = time.strftime("%I-%M-%d") + ".mp4"
 
         # Set out global, defines what video to save to
-        out = cv2.VideoWriter(currFile, cv2.VideoWriter_fourcc(*'MPEG'), 10, (1280,720))
-        print ('recording turned on')
+      ##  out = cv2.VideoWriter(currFile, cv2.VideoWriter_fourcc(*'MPEG'), 10, (1280,720))
+      ##  print ('recording turned on')
 
     return redirect(url_for('index'))
 
