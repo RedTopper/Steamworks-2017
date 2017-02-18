c1e231e8a23bb8e91c8cab89c411500bf0511a0f
Jason Wilcox <wilcoxjaca0@gmail.com>
2017-02-18 17:44:15 -0600

    Recording and bad github
    * Removing old code, adding new python streaming and recording. It works.
    
    * Update README.md
    
    Yup
    
    * Code for RasPi before it gets bagged. This is just a stream
    
    * Not so sketchy code
    
    * Adding back record functionality, because it is done.

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, Response, redirect, url_for
 from camera import Camera
 import cv2
 import time
+from record import Record
 
-cam1 = Camera(0)
-cam2 = Camera(1)
+recorder = Record()
+
+
 
 app = Flask(__name__)
 
@@ -13,10 +15,13 @@ def index():
     
     return render_template('index.html')
 
-def gen(camera):
+
+def gen(num):
+    global recorder
     while True:
 
-        frame = camera.get_frame()
+        frame = recorder.get_curr_frame(num)
+
 
         success, jpeg = cv2.imencode('.jpg', frame)
         yield (b'--frame\r\n'
@@ -26,7 +31,9 @@ def gen(camera):
 @app.route('/video_feed')
 def video_feed():
     global cam1
-    return Response(gen(cam1),
+
+    return Response(gen(1),
+
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # Defines code for the /front_feed path
@@ -34,47 +41,14 @@ def video_feed():
 def front_feed():
 
     global cam2
-    return Response(gen(cam2),
+    return Response(gen(2),
             mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    
-    # Calls the gen method and defines the camera that will be used. DO NOT TOUCH
-    return Response(gen(Camera(1)),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# Defines code for the / path
-@app.route('/')
-def camera_record():
-
-    # When page is first loaded, redirect to the /camera_recording_process_change path
-    return redirect(url_for('camera_recording_process_change'))
-
-# Defines code for the /camera_recording_process_change path
 @app.route('/camera_recording_process_change')
 def camera_recording_process_change():
-    global out, recording, currFile
-
-    # Tag the change in the console
-    print ('This function is turned off...')
-
-    ##if recording:
-
-        # Disable recording
-      ##  recording = False
-      ##  print ('recording turned off')
-        
-    ##else:
-
-        # Enable recording
-      ##  recording = True
-
-        # Set current file global, defines where the video will be saved
-      ##  currFile = time.strftime("%I-%M-%d") + ".mp4"
-
-        # Set out global, defines what video to save to
-      ##  out = cv2.VideoWriter(currFile, cv2.VideoWriter_fourcc(*'MPEG'), 10, (1280,720))
-      ##  print ('recording turned on')
-
+    global recorder
+    recorder.end()
     return redirect(url_for('index'))
 
 # More server code, do not touch                    
