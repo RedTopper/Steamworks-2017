9b814e7c72e8bd706e76015c2c2c8f22d08dde3b
Jason Wilcox <wilcoxjaca0@gmail.com>
2017-02-17 23:45:55 -0600

    IDK
    * Removing old code, adding new python streaming and recording. It works.
    
    * Update README.md
    
    Yup
    
    * Code for RasPi before it gets bagged. This is just a stream
    
    * Not so sketchy code

--- app.py
+++ app.py
@@ -3,48 +3,21 @@ from camera import Camera
 import cv2
 import time
 
-# This is the main application it handles all of the paths as well as what to do with the frames
+cam1 = Camera(0)
+cam2 = Camera(1)
 
-# Globals. If you need thins in different methods, use this
-out = None
-recording = False
-currFile = None
-
-# Do not touch this
 app = Flask(__name__)
 
-# Defines code for the /video_stream path
-@app.route('/video_stream')
+@app.route('/')
 def index():
-
-    # Sets the page to index.html
+    
     return render_template('index.html')
 
 def gen(camera):
-    global out, recording, currFile
     while True:
 
-        # Gets a frame from Camera.py
         frame = camera.get_frame()
 
-        ##recFrame = cv2.resize(frame, (1280,720), interpolation = cv2.INTER_LINEAR)
-        # Any OpenCV image editing that is on the video should go here
-        ##if recording:
-          ##  cv2.putText(frame,"Recording", (15,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,255,255], 2)
-          ##  cv2.putText(recFrame,time.strftime("%I-%M-%S"), (1100,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,255,255], 1)
-
-        cv2.putText(frame,"Score", (15,30), cv2.FONT_HERSHEY_SIMPLEX, 1, [255,255,255], 2)
-        # Write the frame to the video
-        ##out.write(recFrame)
-
-        # Resize the frame so it can be streamed efficently
-        frame = cv2.resize(frame, (640, 480), interpolation = cv2.INTER_LINEAR)
-
-        # If the recording is done, release the file. This is nessisary to not have corrupt video files
-        ##if recording is False:
-          ##  out.release()
-
-        # Streaming code (MJPG Stream)
         success, jpeg = cv2.imencode('.jpg', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
@@ -52,14 +25,18 @@ def gen(camera):
 # Defines code for the /video_feed path
 @app.route('/video_feed')
 def video_feed():
-
-    # Calls the gen method and defines the camera that will be used. DO NOT TOUCH
-    return Response(gen(Camera(0)),
+    global cam1
+    return Response(gen(cam1),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # Defines code for the /front_feed path
 @app.route('/front_feed')
 def front_feed():
+
+    global cam2
+    return Response(gen(cam2),
+            mimetype='multipart/x-mixed-replace; boundary=frame')
+
     
     # Calls the gen method and defines the camera that will be used. DO NOT TOUCH
     return Response(gen(Camera(1)),
@@ -102,4 +79,4 @@ def camera_recording_process_change():
 
 # More server code, do not touch                    
 if __name__ == '__main__':
-    app.run(host='localhost', debug=True)
+    app.run(host='0.0.0.0', debug=False)
