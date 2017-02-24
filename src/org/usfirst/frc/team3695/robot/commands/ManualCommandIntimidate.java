565ffac3e2be7452ad89a585d3e0de5210f545b3
John Doe <ipad3000@yahoo.com>
2017-02-24 12:23:46 -0600

    Finished rough outline. Still hate bells. No triggers. Yet.

--- src/org/usfirst/frc/team3695/robot/commands/ManualCommandIntimidate.java
+++ src/org/usfirst/frc/team3695/robot/commands/ManualCommandIntimidate.java
@@ -13,34 +13,61 @@ public class ManualCommandIntimidate extends Command {
 	Color color;
 	int count;
 	int r,g,b;
+	int speed;
 	
 	public ManualCommandIntimidate() { 
 		requires(Robot.SUB_BLINGY); 
 		r = 0; g = 0; b = 0; count = 0; 
 	}
 
-	protected void initialize() { color = Color.RED; }
+	protected void initialize() { color = Color.RED; speed = 1; }
 	
 	protected void execute() {
-		switch (color) {
-			case RED:
-				
-				break;
-			case ORANGE:
-				
-				break;
-			case YELLOW:
-				
-				break;
-			case GREEN:
-				
-				break;
-			case BLUE:
-				
-				break;
-			case PURPLE:
-				
-				break;
+		for (int i = 0; i < speed; i++) {
+			switch (color) {
+				case RED: //255, 0, 0
+					if (count++ > 255) { color = Color.ORANGE; count = 0; } else {
+						r = 255;
+						g = 0;
+						b--;
+					}
+					break;
+				case ORANGE: //255, 127, 0
+					if (count++ > 255) { color = Color.YELLOW; count = 0; } else {
+						r = 255;
+						g = count / 2;
+						b = 0;
+					}	
+					break;
+				case YELLOW: //255, 255, 0
+					if (count++ > 255) { color = Color.GREEN; count = 0; } else {
+						r = 255;
+						g = (count / 2) + 127;
+						b = 0;
+					}
+					break;
+				case GREEN: //0, 255, 0
+					if (count++ > 255) { color = Color.BLUE; count = 0; } else {
+						r--;
+						g = 255;
+						b = 0;
+					}
+					break;
+				case BLUE: //0, 0, 255
+					if (count++ > 255) { color = Color.PURPLE; count = 0; } else {
+						r = 0;
+						g--;
+						b++;
+					}
+					break;
+				case PURPLE: //255, 0 , 255
+					if (count++ > 255) { color = Color.RED; count = 0; } else {
+						r++;
+						g = 0;
+						b = 255;
+					}
+					break;
+			}
 		}
     	Robot.SUB_BLINGY.RGB(r,g,b);
     }
